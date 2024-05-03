8de7da4e86c7b37d2a10cd3473015b9ae786e46c
João Vitor V <joaovitorzv@outlook.com>
2024-05-03 09:04:35 -0300

    remove route config & use vercel cli 33.7.1

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
 
-export const config = {
-  maxDuration: 5,
-};
-
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
