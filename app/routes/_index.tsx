9cffd193986888c3cb9e362cbc968b8ad553d34c
João Vitor V <joaovitorzv@outlook.com>
2024-05-07 09:32:57 -0300

    test in route config again

---
+++ app/routes/_index.tsx
@@ -0,0 +1,32 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const loader = async () => {
+  const API_URL = "https://catfact.ninja/fact";
+
+  const response = await fetch(API_URL);
+  const data = await response.json();
+
+  return json(data as { fact: string; length: number });
+};
+
+export const meta = () => {
+  return [
+    {
+      title: "Cat Fact At Index",
+    },
+  ];
+};
+
+export default function CatFact() {
+  const { fact, length } = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <h1>Cat Fact at Index</h1>
+      <p>feat: {fact}</p>
+      <p>length: {length}</p>
+    </div>
+  );
+}
